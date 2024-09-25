76f8217f9341f1d687331df850da0657a47b6b66
Suchit02 <suchitmishra1728@gmail.com>
2024-09-25 17:17:24 +0530

    url changes

--- components/page.tsx
+++ components/page.tsx
@@ -28,7 +28,7 @@ const Page = ({ className = "" }) => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://funding4userver-production.up.railway.app/', {
+      const response = await fetch('https://funding4userver-production.up.railway.app/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
